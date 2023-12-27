c0951b82cdda80cc5b4a23a08838475bb5aa28db
T0F1Q2007 <79853089+T0F1Q2007@users.noreply.github.com>
2023-12-27 13:54:57 +0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,30 @@
+navigator.serviceWorker.register('/sw.js')
+.then(function(registration) {
+  console.log('Service worker successfully registered.');
+})
+.catch(function(error) {
+  console.log('Service worker registration failed:', error);
+});
+Notification.requestPermission().then(function(permission) {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+    } else {
+      console.log('Unable to get permission to notify.');
+    }
+});
+navigator.serviceWorker.ready.then(function(registration) {
+    registration.pushManager.subscribe({userVisibleOnly: true})
+    .then(function(subscription) {
+      console.log('Subscribed for push:', subscription.endpoint);
+    })
+    .catch(function(error) {
+      console.log('Subscription failed:', error);
+    });
+});
+self.addEventListener('push', function(event) {
+    const title = 'New Message';
+    const options = {
+      body: 'Hello, world!',
+    };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
