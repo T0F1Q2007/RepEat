c00a38186b7069fef95ed327e6778a9a83502460
T0F1Q2007 <79853089+T0F1Q2007@users.noreply.github.com>
2023-12-28 19:32:59 +0400

    Update for page

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+document.cookie = "Cookie=SweatTomato";
+var date = new Date();
+date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+var expires = "expires=" + date.toUTCString();
+document.cookie = "Cookie=chocolate; " + expires;
 navigator.serviceWorker.register('/sw.js')
 .then(function(registration) {
   console.log('Service worker successfully registered.');
